20170e389ec858c765fb6a4efa6ded08b83875e7
yueranlu <yueranlu05@gmail.com>
2026-05-23 17:18:59 -0400

    feat: fish swim

--- js/fish.js
+++ js/fish.js
@@ -1,112 +1,129 @@
-const fishCanvas = document.getElementById('fishCanvas');
-const spawnBtn = document.getElementById('spawnBtn');
-const fishName = document.getElementById('fishName');
-const drawCanvas = document.getElementById('drawCanvas');
-const drawOverlay = document.getElementById('drawOverlay');
-const drawCtx = drawCanvas.getContext('2d')
+const fishCanvas = document.getElementById("fishCanvas");
+const spawnBtn = document.getElementById("spawnBtn");
+const fishName = document.getElementById("fishName");
+const drawCanvas = document.getElementById("drawCanvas");
+const drawOverlay = document.getElementById("drawOverlay");
+const drawCtx = drawCanvas.getContext("2d");
 
-const fishCtx = fishCanvas.getContext('2d');
+const fishCtx = fishCanvas.getContext("2d");
 
 fishCanvas.width = window.innerWidth;
 fishCanvas.height = window.innerHeight;
 
-window.addEventListener('resize', function(){
-    fishCanvas.width = window.innerWidth;
-    fishCanvas.height = window.innerHeight;
+window.addEventListener("resize", function () {
+  fishCanvas.width = window.innerWidth;
+  fishCanvas.height = window.innerHeight;
 });
 
 // fish array to store fish objects
 const fishArray = [];
 
 //spawn button for the fish
-spawnBtn.addEventListener('click', function(){
-    const name = fishName.value || "Fih"
-    const dataURL = drawCanvas.toDataURL('image/png'); //converst user drawing to the encoded png
-    const img = new Image();
-    img.src = dataURL;
+spawnBtn.addEventListener("click", function () {
+  const name = fishName.value || "Fih";
+  const dataURL = drawCanvas.toDataURL("image/png"); //converst user drawing to the encoded png
+  const img = new Image();
+  img.src = dataURL;
 
-    // all values needed for the fish object
-    img.onload = function(){
+  // all values needed for the fish object
+  img.onload = function () {
     //random span point
-        const x = 100 + Math.random() * (fishCanvas.width - 200);
-        const targetSpawnY= Math.random() * (fishCanvas.height - 200) + 50; // starts from random y position, but dumber between canvas (hight -200)+ 50, so it't not too close to the edge
-        const y = -100;
+    const x = 100 + Math.random() * (fishCanvas.width - 200);
+    const targetSpawnY = Math.random() * (fishCanvas.height - 200) + 50; // starts from random y position, but dumber between canvas (hight -200)+ 50, so it't not too close to the edge
+    const y = -100;
 
-        //pick a random destiation for each fish
-        const destX = Math.random() * (fishCanvas.width - 100) + 50;
-        const destY = Math.random() * (fishCanvas.height - 150) + 50;
+    //pick a random destiation for each fish
+    const destX = Math.random() * (fishCanvas.width - 100) + 50;
+    const destY = Math.random() * (fishCanvas.height - 150) + 50;
 
-        //pick random speeds
-        const speed = Math.random() * 2 + 0.5 // speed between 0.5 and 2.5
-        const wobbleOffset = Math.random() * Math.PI * 2
-        const wobbleSpeed = Math.random() * 0.003 + 0.001
-        const wobbleAmount = Math.random() * 0.7 + 0.3
+    //pick random speeds
+    const speed = Math.random() * 2 + 0.5; // speed between 0.5 and 2.5
+    const wobbleOffset = Math.random() * Math.PI * 2;
+    const wobbleSpeed = Math.random() * 0.003 + 0.001;
+    const wobbleAmount = Math.random() * 0.7 + 0.3;
 
-        const fishWidth = Math.random() * 40 + 80
-        const fishHeight = fishWidth * (drawCanvas.height / drawCanvas.width);
+    const fishWidth = Math.random() * 40 + 80;
+    const fishHeight = fishWidth * (drawCanvas.height / drawCanvas.width);
 
-        // creating fish object
-        const fish = {
-        image: img,
-        name: name,
-        x: x,
-        y: y,
-        destX: destX,
-        destY: destY,
-        speed: speed,
-        wobbleOffset: wobbleOffset,
-        wobbleSpeed: wobbleSpeed,
-        wobbleAmount: wobbleAmount,
-        targetX: null,
-        targetY: null,
-        opacity: 0,
-        spawning: true,
-        velocityY: 0,
-        targetSpawnY: targetSpawnY,
-        width: fishWidth,
-        height: fishHeight,
+    // creating fish object
+    const fish = {
+      image: img,
+      name: name,
+      x: x,
+      y: y,
+      destX: destX,
+      destY: destY,
+      speed: speed,
+      wobbleOffset: wobbleOffset,
+      wobbleSpeed: wobbleSpeed,
+      wobbleAmount: wobbleAmount,
+      targetX: null,
+      targetY: null,
+      opacity: 0,
+      spawning: true,
+      velocityY: 0,
+      targetSpawnY: targetSpawnY,
+      width: fishWidth,
+      height: fishHeight,
+    };
+    fishArray.push(fish);
+
+    // now clear canvas
+    drawOverlay.style.display = "none";
+    drawCtx.clearRect(0, 0, drawCanvas.width, drawCanvas.height);
+    drawCtx.fillStyle = "white";
+    drawCtx.fillRect(0, 0, drawCanvas.width, drawCanvas.height);
+    fishName.value = "";
+  };
+});
+
+// so in the niamtion loop it happends 60 times in a econd
+// it checks each fish to see if it is sawning or not,
+// if it is it checks whttht eopacity should be using and sets opacity to that
+// sets the en that draws that fish to 0.4 and then resert it to 1 for the next fish
+function animate() {
+  fishCtx.clearRect(0, 0, fishCanvas.width, fishCanvas.height);
+  for (i = 0; i < fishArray.length; i++) {
+    const fih = fishArray[i];
+    // animation when fish is falling and spawning into the tank
+    if (fih.spawning == true) {
+      if (fih.opacity < 1) {
+        fih.opacity += 0.03;
+        fih.velocityY += 0.3;
+        fih.y += fih.velocityY;
+      }
+
+      //check is fish has reached it;s corresponding y
+      if (fih.y >= fih.targetSpawnY) {
+        fih.y = fih.targetSpawnY;
+        fih.velocity *= -0.4; // this will bounce the fish
+        if (Math.abs(fih.velocity) < 0.5) {
+          fih.spawning = false;
+          fih.opacity = 1;
+          fih.y = targetSpawnY;
         }
-        fishArray.push(fish)
+      }
+    } else if (fih.spawning == false) {
+      // find distance and direction to the next point
+      const dx = fish.destX - fish.x;
+      const dy = fish.destY - fish.y;
+      const distance = Math.sqrt(dx * d + dy * dy);
+      //move the fish towards the point so update fish's position using the fish's speed
+      fish.x += (dx / distance) * fish.speed;
+      fish.y += (dy / distance) * fish.speed;
+      // add a wobble
+      fish.y +=
+        Math.sin(Date.now() * fish.wobbleSpeed + fish.wobbleOffset) *
+        fish.wobbleAmount;
+      //pick new destintation when fish arrives
 
-        // now clear canvas
-        drawOverlay.style.display = 'none';
-        drawCtx.clearRect(0, 0, drawCanvas.width, drawCanvas.height);
-        drawCtx.fillStyle = 'white';
-        drawCtx.fillRect(0, 0, drawCanvas.width, drawCanvas.height);
-        fishName.value = '';
+      if (distance < 20) {
+        fish.destX = 50 + Math.random() * (fishCanvas.width - 100);
+        fish.destY = 50 + Math.random() * (fishCanvas.height - 150);
+      }
     }
-
-})
-
-
-function animate(){
-    fishCtx.clearRect(0,0,fishCanvas.width, fishCanvas.height)
-    for (i = 0; i < fishArray.length; i++){
-        const fih = fishArray[i];
-        if (fih.spawning == true){
-            if (fih.opacity < 1){
-                fh.opacity += 0.03;
-                fih.velocityY += 0.3;
-                fih.y += fih.velocityY;
-            }
-
-            //check is fish has reached it;s corresponding y
-            if (fih.y >= fish.tagetSpawnY){
-                fish.y = fish.targetSpawnY;
-                fish.velocity *= -0.4; // this will bounce the fish
-                if (Math.abs(fish.velocity) < 0.5){
-                    fish.spawning = false;
-                    fish.opacity = 1;
-                    fish.y = targetSpawnY;
-                }
-            }
-        }
-
-        if (fih.spawning == false){
-            
-
-        }
-    }
-
-
+    // reset pen to 1 for next fish
+    fishCtx.globalAlpha = fish.opacity;
+    fishCtc.globalAplha = 1;
+  }
 }
